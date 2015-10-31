e56ca4def72e06f6731d10f59ceb94f272666591
Chris Murphy <christmurphy@gmail.com>
2015-10-31 14:21:56 -0600

    got some movement and stuff

--- TowerDefense/GameScene.swift
+++ TowerDefense/GameScene.swift
@@ -7,15 +7,18 @@
 //
 
 import SpriteKit
-
+import UIKit
 
 
 class GameScene: SKScene , SKPhysicsContactDelegate{
      let satellite = SKSpriteNode(imageNamed: "Sat2")
      let myLabel = SKLabelNode(fontNamed:"Verdana")
+    let viewTime = UIView(frame: CGRectMake(0, 0, 200, 200))
+    var b = UIButton(frame: CGRectMake(0,0, 200,10))
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        physicsWorld.gravity = CGVectorMake(0,-1.2)
+        physicsWorld.gravity = CGVectorMake(0,0)
         physicsWorld.contactDelegate = self
 
         myLabel.text = "DEFFEND!";
@@ -24,8 +27,19 @@ class GameScene: SKScene , SKPhysicsContactDelegate{
         
         self.addChild(myLabel)
         
+        
+        b.setTitle("yeah", forState: UIControlState.Normal)
+        b.setTitleColor(UIColor.brownColor(), forState: UIControlState.Normal)
+        b.addTarget(self, action: "buttonTime", forControlEvents: UIControlEvents.TouchUpInside)
+        viewTime.addSubview(b)
+        
+        self.view?.addSubview(viewTime)
     }
-    
+    func buttonTime()
+    {
+        b.setTitle("nope", forState: UIControlState.Normal)
+    }
+    //
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
@@ -40,7 +54,7 @@ class GameScene: SKScene , SKPhysicsContactDelegate{
             sprite.xScale = 0.5
             sprite.yScale = 0.5
             sprite.position = location
-        let vect = CGVector(dx: CGFloat(450), dy: CGFloat(203))
+        let vect = CGVector(dx: CGFloat(-450), dy: CGFloat(0))
             sprite.physicsBody = SKPhysicsBody()
         sprite.physicsBody?.collisionBitMask = PhysicsCategory.Tower
         sprite.physicsBody?.dynamic = true
---
+++ TowerDefense/TowerBase.swift
@@ -0,0 +1,15 @@
+//
+//  TowerBase.swift
+//  TowerDefense
+//
+//  Created by Chris Murphy on 10/29/15.
+//  Copyright © 2015 Chris Murphy. All rights reserved.
+//
+
+import Foundation
+
+class BaseTower {
+    var health = 0
+    
+    
+}
