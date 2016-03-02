45292c0501ca343e2bad93f0f5e13ee494f91406
Tobias Kundig <Kundigt@gmail.com>
2016-03-01 18:49:10 -0700

    Getting SideScroll Scene under control. Dev branch

--- TowerDefense/SideScrolScene.swift
+++ TowerDefense/SideScrolScene.swift
@@ -21,12 +21,16 @@ class SideScrolScene: SKScene , SKPhysicsContactDelegate{
     var midgroundNode: SKNode = SKNode()
     var foregroundNode: SKNode = SKNode()
     var hudNode: SKNode = SKNode()
-    
+    let shipNode = SKNode()
     var scaleFactor: CGFloat = 0.0
     var ship: SKNode = SKNode()
-    var actionMoveUp = SKAction()
-    var actionMoveDown = SKAction()
-    let backgroupVelocity: CGFloat = 3.0
+    
+    let shipSprite = SKSpriteNode(imageNamed: "Spaceship")
+    let backgroundVelocity: CGFloat = 2.0
+    
+    //Enemy Factory
+    var enemyFactory = EnemyFactory()
+    var towerBuilder = TowerBuilder()
     
     static var ships : [TowerBase] =  [TowerBase]() // Stores all towers in level in order to call their strategies each frame
     static var enemies : [EnemyBase] = [EnemyBase]() // Stores all towers in level in order to call their strategies each frame
@@ -34,32 +38,29 @@ class SideScrolScene: SKScene , SKPhysicsContactDelegate{
     
     override func didMoveToView(view: SKView) {
         
-        //backgroundNode = createBackgroundNode()
-        foregroundNode = SKNode()
-        ship = createShip()
-        foregroundNode.addChild(ship)
-        scaleFactor = 3.0
-        
 
-        //self.addChild(backgroundNode)
+        scaleFactor = 3.0
         
         physicsWorld.gravity = CGVectorMake(0,0)
         physicsWorld.contactDelegate = self
         self.view!.multipleTouchEnabled = true
         self.backgroundColor = SKColor.blackColor()
-        self.initializingScrollingBackground()
-        self.addChild(foregroundNode)
+        //self.initializingScrollingBackground()
+        
+        createShip()
+        //foregroundNode.addChild(ship)
+        SideScrolScene.scene?.addChild(shipSprite)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
 
         for touch: AnyObject in touches {
             let location = touch.locationInNode(self)
-            if location.y > ship.position.y {
-                ship.runAction(actionMoveUp)
+            if location.y > shipSprite.position.y {
+                moveUp()
             }
             else{
-                ship.runAction(actionMoveDown)
+                moveDown()
             }
         }
     }
@@ -70,40 +71,47 @@ class SideScrolScene: SKScene , SKPhysicsContactDelegate{
     }
     
     func createShip() -> SKNode {
-        let shipNode = SKNode()
-        shipNode.position = CGPoint(x: 100, y: 0)
+
+        //shipNode.position = CGPoint(x: 200, y: 200)
+        shipSprite.position = CGPoint(x: 200, y: 200)
+        shipSprite.xScale = 0.1
+        shipSprite.yScale = 0.1
+        shipSprite.physicsBody?.dynamic = true
+        shipSprite.physicsBody?.mass = 1
+        shipSprite.physicsBody?.restitution = 1.0
+        shipSprite.physicsBody?.linearDamping = 1.0
+        shipSprite.physicsBody?.angularDamping = 1.0
+        shipSprite.physicsBody?.allowsRotation = false
+        shipSprite.zPosition = ZPosition.bullet
+        shipSprite.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(0.1, 0.1))
+
+        shipSprite.zRotation = CGFloat(-M_PI/2)
         
-        let sprite = SKSpriteNode(imageNamed: "Spaceship")
-        sprite.xScale = 0.5
-        sprite.yScale = 0.5
+        //shipNode.addChild(shipSprite)
         
-        shipNode.addChild(sprite)
-        actionMoveUp = SKAction.moveByX(0, y: 30, duration: 0.1)
-        actionMoveDown = SKAction.moveByX(0, y: -30, duration: 0.1)
-        
-        return shipNode
+        return shipSprite
     }
-    
-    func createBackgroundNode() -> SKNode {
+    func moveUp() {
+        shipSprite.physicsBody?.applyImpulse(CGVectorMake(0.0, 0.01))
+    }
+    func moveDown() {
+        shipSprite.physicsBody?.applyImpulse(CGVectorMake(0.0, -0.01))
+    }
+    func createMidgroundNode() -> SKNode {
         
         let ySpacing = 64.0 * scaleFactor
         
-        for index in 0...3 {
-            let node = SKSpriteNode(imageNamed:String("BackgroundSide", index + 1))
-            
-            node.setScale(scaleFactor)
-            node.anchorPoint = CGPoint(x: 0.5, y: 0.0)
-            node.position = CGPoint(x: self.size.width / 2, y: ySpacing * CGFloat(index))
-            
-            backgroundNode.addChild(node)
-        }
+        let node = SKSpriteNode(imageNamed:String("Rock2"))
+
+        backgroundNode.addChild(node)
+        
         return backgroundNode
     }
     
     func moveBackground(){
         self.enumerateChildNodesWithName("background", usingBlock: {(node, stop) -> Void in
             if let bg = node as? SKSpriteNode {
-                bg.position = CGPoint(x: bg.position.x - self.backgroupVelocity, y: bg.position.y)
+                bg.position = CGPoint(x: bg.position.x - self.backgroundVelocity, y: bg.position.y)
                 
                 if bg.position.x <= -bg.size.width {
                     bg.position = CGPointMake(bg.position.x + bg.size.width * 2, bg.position.y)
@@ -114,10 +122,11 @@ class SideScrolScene: SKScene , SKPhysicsContactDelegate{
     }
     func initializingScrollingBackground() {
         for var index = 0; index < 15; ++index{
-            let bg = SKSpriteNode(imageNamed: "gridBG")
+            let bg = SKSpriteNode(imageNamed: "SideScrollbg")
             bg.position = CGPoint(x: index * Int(bg.size.width), y: 0)
             bg.anchorPoint = CGPointZero
             bg.name = "background"
+            bg.zPosition = ZPosition.background
             self.addChild(bg)
             
         }
