431d777f42d94d5afdb7f342ae440da0a8d8269d
aaroncameron21 <aaroncameron21@gmail.com>
2016-03-11 14:02:04 -0700

    Update to planet discovery

--- TowerDefense/Planet.swift
+++ TowerDefense/Planet.swift
@@ -14,13 +14,34 @@ class Planet {
     
     
     var circle : SKShapeNode?
+    var position : CGPoint;
+    var size : CGFloat;
+    var color : SKColor
+    var discovered : Bool = false;
+    
+    var metal : CGFloat = 0.0;
+
     
     
     init (size : CGFloat, position : CGPoint, color : SKColor) {
+        self.position = position;
+        self.size = size;
+        self.color = color;
+        //self.discover();
+    }
+    
+    func checkDiscovery(distance: CGFloat, position: CGPoint) {
+        if (getDistance(self.position, to: position) < distance) {
+            discover();
+        }
+    }
+    
+    func discover() {
+        discovered = true;
         circle = SKShapeNode(circleOfRadius: size)
         circle?.position = position;
-        circle?.lineWidth = 1.0;
-        circle?.glowWidth = size/4.0;
+        circle?.lineWidth = 0.0;
+        circle?.glowWidth = 0.0;
         
         // Color fades as pulse progresses
         circle?.fillColor = color;
@@ -29,6 +50,11 @@ class Planet {
         circle?.zPosition = ZPosition.tower-1;
         
         // Add circle back to scene
-        PlanetPickScene.scene?.addChild(circle!)    }
+        PlanetPickScene.scene?.addChild(circle!)
+    }
+    
+    func update(offset : CGPoint) {
+        circle?.position = CGPoint(x: self.position.x + offset.x, y: self.position.y + offset.y);
+    }
     
 }
--- TowerDefense/PlanetPickScene.swift
+++ TowerDefense/PlanetPickScene.swift
@@ -17,6 +17,11 @@ class PlanetPickScene: SKScene , SKPhysicsContactDelegate{
     var viewController: PlanetPickView!
     
     var backgroundNode: SKNode = SKNode()
+    
+    var lastTouchPosition: CGPoint = CGPoint(x: 0.0, y: 0.0);
+    
+    var offset : CGPoint = CGPoint(x: 0.0, y: 0.0);
+    
     //var midgroundNode: SKNode = SKNode()
     //var foregroundNode: SKNode = SKNode()
     //var hudNode: SKNode = SKNode()
@@ -37,26 +42,72 @@ class PlanetPickScene: SKScene , SKPhysicsContactDelegate{
             PlanetPickScene.planets.append(Planet(size: 10 + CGFloat(arc4random_uniform(10)), position: getPlanetPosition(), color: getRandomColor()));
         }
         
+        newDiscovery(1000, y: 1000, r: 1000);
         
         
+        
+    }
+    
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for t in touches {
+            self.newDiscovery(t.locationInNode(self).x, y: t.locationInNode(self).y, r: 100);
+            break;
+        }
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+//        for t in touches {
+//            let deltaP : CGPoint = CGPoint(x: t.locationInNode(self).x-lastTouchPosition.x, y: t.locationInNode(self).y-lastTouchPosition.y);
+//            offset.x += deltaP.x;
+//            offset.y += deltaP.y;
+//            lastTouchPosition = t.locationInNode(self);
+//            break;
+//        }
+//        for p in PlanetPickScene.planets {
+//            p.update(offset);
+//        }
+    }
+    
+    override func update(currentTime: CFTimeInterval) {
+//        for p in PlanetPickScene.planets {
+//            p.update(offset);
+//        }
     }
     
     func getPlanetPosition() -> CGPoint{
         
+        // 0 -> 2000
         
         let gridX = CGFloat(arc4random_uniform(50));
         let gridY = CGFloat(arc4random_uniform(50));
-        return CGPoint(x: (gridX * 30), y: (gridY * 30));
+        return CGPoint(x: (gridX * 40), y: (gridY * 40));
     }
     
     func getRandomColor() -> SKColor {
-        var randomRed:CGFloat = CGFloat(drand48())
+        let randomHue: CGFloat = CGFloat(drand48())
+        return SKColor(hue: randomHue, saturation: 1.0, brightness: 1.0, alpha: 1.0);
+    }
+    
+    func newDiscovery(x: CGFloat, y: CGFloat, r: CGFloat) {
+        let circle : SKShapeNode = SKShapeNode(circleOfRadius: r);
+        circle.position = CGPoint(x: x, y: y);
+        circle.lineWidth = 0.0;
+        circle.glowWidth = 0.0;
         
-        var randomGreen:CGFloat = CGFloat(drand48())
+        // Color fades as pulse progresses
+        circle.fillColor = SKColor(red: 0.0, green: 0.0, blue: 0.2, alpha: 1.0);
+        circle.strokeColor = SKColor.whiteColor();
         
-        var randomBlue:CGFloat = CGFloat(drand48())
+        circle.zPosition = ZPosition.tower-10;
         
-        return SKColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0);
+        // Add circle back to scene
+        PlanetPickScene.scene?.addChild(circle)
+        
+        for p in PlanetPickScene.planets {
+            p.checkDiscovery(r, position: CGPoint(x: x, y: y));
+        }
     }
 
 }
